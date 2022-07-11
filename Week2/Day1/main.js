587666c6cd6cf542d7bbd36f9c368b24bb5295e2
pratikshasabale <sabalepratiksha27@gmail.com>
2022-07-11 16:44:18 +0530

    add file

---
+++ Week2/Day1/main.js
@@ -0,0 +1,12 @@
+function create() {
+    
+    let email=document.getElementById("email").value; 
+    let studentname=document.getElementById("studentname").value;
+    let collegename=document.getElementById("collegename").value;
+    let branch=document.getElementById("branch").value;
+    let add=document.getElementById("add").value;
+    let mobilenumber=document.getElementById("mobilenumber").value;
+    
+    document.write(email+"</br>" + studentname+"</br>" + collegename+"</br>" + branch+"</br>" + add+"</br>" + mobilenumber+"</br>")    
+    
+}
